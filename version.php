e9b2513c8305c0af21d0a36ab5a812eb6fcf37a7
Andre Scherl <post@schoerlock.de>
2012-08-20 22:46:19 +0200

    completed version file

--- version.php
+++ version.php
@@ -1,4 +1,7 @@
 <?php
-	$plugin->version  = 20120716001; // Plugin version
+	$plugin->version  = 2012071601; // Plugin version
 	$plugin->cron = 1; // Set min time between cron executions in seconds
-	
+	$plugin->requires = 2010112400; // Moodle 2.0
+	$plugin->component = 'block_case_repository';
+	$plugin->release = '1.0 (Build: 2012071601)';
+	$plugin->dependencies = array('block_semantic_web' => 2012071601, 'block_user_preferences' => 2011090600);
